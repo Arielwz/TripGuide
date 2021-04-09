e495ac941a11b3f473e0d36144d3f9f72f23caaa
Arielwz <arieldududu@gmail.com>
2021-04-08 18:03:55 -0700

    change authen.css and packege.json

--- db/MyDB.js
+++ db/MyDB.js
@@ -22,6 +22,49 @@ function MyDB() {
     }
   };
 
+  // creat users info
+  myDB.creatUser = async (user) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true }); //connect to mogoClient
+      console.log("Connecting to the db");
+      await client.connect();
+      console.log("Connected!");
+      const res = await client
+        .db(DB_NAME)
+        .collection("usersinfo")
+        .insertOne(user);
+      console.log("Inserted", res);
+
+      return res;
+    } finally {
+      console.log("Closing the connection");
+      client.close();
+    }
+  };
+
+  // search user info
+  myDB.searchUser = async (query) => {
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true }); //connect to mogoClient
+      console.log("Connecting to the db");
+      await client.connect(); // establish a connection to the server
+      console.log("Connected!");
+      const db = client.db(DB_NAME); //select specific database
+      const usersCol = db.collection("usersinfo"); //get the collection
+
+      console.log("Collection ready, querying with ", query);
+      const users = await usersCol.find(query).toArray(); //get users in the collection
+      console.log("Got users", users);
+
+      return users;
+    } finally {
+      console.log("Closing the connection");
+      client.close();
+    }
+  };
+
   return myDB;
 }
 
